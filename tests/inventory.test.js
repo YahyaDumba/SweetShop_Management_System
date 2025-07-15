b876599f5e89703c5d991bea47817047b6e9a86f
YahyaDumba <yahyadumba63@gmail.com>
2025-07-16 02:34:57 +0530

    Fix: Corrected Sweet IDs by Expectations in delete and searchByName tests passing

--- tests/inventory.test.js
+++ tests/inventory.test.js
@@ -4,7 +4,7 @@ const Sweet = require('../src/Sweet.js');
 
 test('should add a sweet to the Inventory',()=>{
     const inventory = new Inventory();
-    const sweet = new Sweet(1001, 'Chocolate','Dark', 100, 100);
+    const sweet = new Sweet(1001, 'Kaju Katli','Nut-Based', 50, 20);
     inventory.addSweet(sweet);
     const sweets = inventory.viewSweets();
     expect(sweets.length).toBe(1);
@@ -15,17 +15,17 @@ test('should add a sweet to the Inventory',()=>{
 test('should delete a sweet from the Inventory',()=>{
     const inventory = new Inventory();
 
-    const sweet1 = new Sweet(1, 'Chocolate','Dark', 100, 100);
-    const sweet2 = new Sweet(2, 'Chocolate','Silk', 200, 50);
+    const sweet1 = new Sweet(1001, 'Kaju Katli','Nut-Based', 50, 20);
+    const sweet2 = new Sweet(1002, 'Gajar Halwa','Vegetable-Based', 30, 15);
 
     inventory.addSweet(sweet1);
     inventory.addSweet(sweet2);
 
-    inventory.deleteSweet(1); //Deleting Sweet on the basis of ID
+    inventory.deleteSweet(1001); //Deleting Sweet on the basis of ID
 
     const sweets = inventory.viewSweets();
     expect(sweets.length).toBe(1);
-    expect(sweets[0].id).toBe(2);
+    expect(sweets[0].id).toBe(1002);
 })
 
 
@@ -34,9 +34,9 @@ test('should search sweets by name',()=>{
     const inventory = new Inventory();
 
     const sweets = [
-        new Sweet(1, 'Chocolate','Dark', 100, 100),
-        new Sweet(2, 'Chocolate','Silk', 200, 50),
-        new Sweet(3, 'Kaju Katli','Nut-Based', 350, 10)
+        new Sweet(1001, 'Kaju Katli','Nut-Based', 50, 20),
+        new Sweet(1002, 'Gajar Halwa','Vegetable-Based', 30, 15),
+        new Sweet(1003, 'Kaju Katli','Milk-Based', 10, 50)
     ];
     
     
@@ -44,6 +44,6 @@ test('should search sweets by name',()=>{
 
     const result = inventory.searchByName('Kaju Katli');
 
-    expect(result.length).toBe(1);
+    expect(result.length).toBe(2);
     expect(result[0].name).toBe('Kaju Katli');
 });
